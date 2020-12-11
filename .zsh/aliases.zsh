3dd21a25153fe83da80ed7f6503b1334da96d441
JA Samitier <jesus.samitier@caseonit.com>
2020-12-11 12:39:54 +0100

    clean zsh aliases, protobuf syntax for vim
    and update tmuxinator config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,4 +1,3 @@
-# alias vim="vimr -s"
 alias vim="nvim"
 alias ivm="vim"
 alias weather='curl -4 wttr.in/Zaragoza\?lang=es'
@@ -10,13 +9,12 @@ alias grep='grep --color'
 alias kdiff3='/Applications/kdiff3.app/Contents/MacOS/kdiff3'
 alias mergetool='git mergetool --tool kdiff3'
 alias difftool='git difftool --tool kdiff3'
-# alias diff="diff-so-fancy"
+alias diff="diff-so-fancy"
 # alias cat="bat"
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 #alias ls="exa"
-#alias l="exa -lahF"
+alias l="exa -lahF"
 #alias find="fd"
 alias tmux="tmux -u"
 alias k="kubectl"
 alias mux="tmuxinator"
-alias df="dotfiles"
