5a9af59d076d695c12527058c2166c8a0a979d03
Jesus Samitier <contacto@jasamitier.com>
2023-02-12 11:20:58 +0100

    change font to JetBrainsMono

---
+++ vscode/Library/Application Support/Code/User/install_extensions.sh
@@ -0,0 +1,6 @@
+code --install-extension Catppuccin.catppuccin-vsc
+code --install-extension chunsen.bracket-select
+code --install-extension ms-python.isort
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension zhuangtongfa.material-theme
