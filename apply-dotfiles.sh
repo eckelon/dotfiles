555d58a21068ef3cef6b3da849addd3c34ee2211
JA Samitier <contacto@jasamitier.com>
2024-01-07 03:22:05 +0100

    add vscodium

--- apply-dotfiles.sh
+++ apply-dotfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for symlink in zsh brew git helix karabiner kitty raycast tmux vim vscode starship wezterm nvim
+for symlink in zsh brew git helix karabiner kitty raycast tmux vim vscodium starship wezterm nvim
 do
   stow -v -t "$HOME" "$symlink"
 done
