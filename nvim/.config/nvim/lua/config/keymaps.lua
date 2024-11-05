2f173bb50789f6b6da15f525700bddbb8a24a7bb
JA Samitier <jesus.samitier@sysdig.com>
2024-11-05 08:14:05 +0100

    nvim: custom keymaps and typescript support

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jj", "<esc>")
+vim.keymap.set("i", "jj", "<esc>", { desc = "Go to normal mode" })
 
 -- I <3 Helix
-vim.keymap.set("n", "gh", "0")
-vim.keymap.set("n", "gl", "$")
-vim.keymap.set("n", "ge", "G")
+vim.keymap.set("n", "gh", "0", { desc = "Start of line" })
+vim.keymap.set("n", "gl", "$", { desc = "End of line" })
+vim.keymap.set("n", "ge", "G", { desc = "Last line" })
