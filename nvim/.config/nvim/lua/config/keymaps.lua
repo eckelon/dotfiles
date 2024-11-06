41ed0e8c2aada828d881f4f159c3925f74338504
JA Samitier <jesus.samitier@sysdig.com>
2024-11-06 18:31:54 +0100

    nvim - map control+c to comment line/block

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jj", "<esc>", { desc = "Go to normal mode" })
+vim.api.nvim_set_keymap("i", "jj", "<esc>", { desc = "Go to normal mode" })
 
 -- I <3 Helix
-vim.keymap.set("n", "gh", "0", { desc = "Start of line" })
-vim.keymap.set("n", "gl", "$", { desc = "End of line" })
-vim.keymap.set("n", "ge", "G", { desc = "Last line" })
+vim.api.nvim_set_keymap("n", "gh", "0", { desc = "Start of line" })
+vim.api.nvim_set_keymap("n", "gl", "$", { desc = "End of line" })
+vim.api.nvim_set_keymap("n", "ge", "G", { desc = "Last line" })
+
+-- Map Ctrl-C to toggle comments
+vim.api.nvim_set_keymap("n", "<C-c>", "gcc", { noremap = false, silent = true })
+vim.api.nvim_set_keymap("v", "<C-c>", "gc", { noremap = false, silent = true })
