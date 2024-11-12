b02908bd2e8dc9a149876579c61f2dd248bd9054
Jesus Samitier <jesus.samitier@sysdig.com>
2024-11-12 11:42:47 +0100

    super tab in nvim, delta in git

--- nvim/.config/nvim/lua/plugins/custom-plugins.lua
+++ nvim/.config/nvim/lua/plugins/custom-plugins.lua
@@ -4,8 +4,68 @@ return {
     enabled = false,
   },
   {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+  {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = { style = "storm" },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "-", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader><space>", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
+      { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    --- Use Supertab like completion
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+            cmp.select_next_item()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
   },
 }
